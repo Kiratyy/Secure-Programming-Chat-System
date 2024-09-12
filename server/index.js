5db28f79698f63b29c1965c8ad2e99e631ee1cb4
a1868575 <109933704+a1868575@users.noreply.github.com>
2024-09-13 04:49:44 +0930

    Update index.js to serve landing.html as default

--- server/index.js
+++ server/index.js
@@ -6,8 +6,13 @@ const port = 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Route for the home page
+// Route for the landing page
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'landing.html'));
+});
+
+// Route for the home chat page
+app.get('/main.html', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'main.html'));
 });
 
